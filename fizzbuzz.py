df62bd48b51ec20ddb90a68fda6a09e2bca38f20
jatkin-wasti <jatkin-wasti@spartaglobal.com>
2020-11-05 14:17:49 +0000

    created fizz and buzz files

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,3 @@
-
 class FizzBuzz:
     def __init__(self):
         self.fizzbuzz = [3, 5]
@@ -6,5 +5,9 @@ class FizzBuzz:
     def fizzbuzz_checker(self, num):
         if (num % self.fizzbuzz[0] == 0) and (num % self.fizzbuzz[1] == 0):
             return "FizzBuzz"
+        else:
+            return num
 
 
+test = FizzBuzz()
+print(test.fizzbuzz_checker(30))
